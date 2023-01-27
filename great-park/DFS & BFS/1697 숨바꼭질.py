a73858a4e37e4e2dcfadec961be1ebbb8ffef00c
great-park <cksgh1735@gmail.com>
2023-01-27 09:20:37 +0900

    코드 수정

--- great-park/DFS & BFS/1697 숨바꼭질.py
+++ great-park/DFS & BFS/1697 숨바꼭질.py
@@ -17,16 +17,14 @@ def BFS():
     
     # 만약 0이 아니라면, 이미 "다른 경로"로 해당 자리를 거쳤고,
     # 그 자리 값에 1을 더하는 행위는 곧, 현재의 경로가 최단 경로가 아님을 의미한다.
-    # 순간이동하는 경우가 이득일 때만 실시
-    if abs(K - 2*a) < abs(K - a) and graph[2*a] == 0:
-      queue.append(2*a)
-      graph[2*a] = graph[a]+1
-    # 걷기
-    if a+1 <= MAX and graph[a+1] == 0:
+    if 0 <= a-1 < MAX + 1 and graph[a-1] == 0:
+      queue.append(a-1)
+      graph[a-1] = graph[a]+1    
+    if 0 <= a+1 < MAX + 1 and graph[a+1] == 0:
       queue.append(a+1)
       graph[a+1] = graph[a]+1
-    if a-1 >= 0 and graph[a-1] == 0:
-      queue.append(a-1)
-      graph[a-1] = graph[a]+1
+    if 0 <= 2*a < MAX+1 and graph[2*a] == 0:
+      queue.append(2*a)
+      graph[2*a] = graph[a]+1
 
 BFS()
