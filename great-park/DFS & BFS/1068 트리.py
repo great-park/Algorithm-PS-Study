414678004424b71fde7a44a505533a7e94835835
great-park <cksgh1735@gmail.com>
2023-01-28 14:09:55 +0900

    feat: Day4 - DFS&BFS BOJ_1068 성공

--- great-park/DFS & BFS/1068 트리.py
+++ great-park/DFS & BFS/1068 트리.py
@@ -10,11 +10,15 @@ Removed = int(input())
 tree = [[] for _ in range(N)]
 # 생존 여부
 alive = [True for _ in range(N)]
+# root 노드 인덱스
+root = 0
 
 
 for i in range(N):
     if parent_info[i] != -1:
         tree[parent_info[i]].append(i)  # 자식 추가
+    else:
+        root = i
 
 
 def BFS(p):
@@ -38,8 +42,11 @@ def solve(removed_node):
     cnt = 0
     alive[removed_node] = False
 
+    # root 노드를 삭제하는 경우
+    if removed_node == root:
+        return 0
     # 리프노드를 삭제하는 경우
-    if len(tree[removed_node]) == 0:
+    elif len(tree[removed_node]) == 0:
         # 삭제하려는 노드의 부모 인덱스 구하기
         parent_index = 0
         for i, parent in enumerate(tree):
