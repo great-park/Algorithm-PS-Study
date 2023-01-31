e9732fe7ac8984ee880c7e7ba32ce5dde0e2873a
yummygyudon <bang2brew@gmail.com>
2023-01-31 18:07:11 +0900

    CHORE : 이전 풀이들 코드 구조 및 풀이 방식 추가

--- yummygyudon/DFS+BFS/day5/10026_적록색약.py
+++ yummygyudon/DFS+BFS/day5/10026_적록색약.py
@@ -7,83 +7,84 @@ d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 N = int(input())
 
 """
-방식 1 : 함수 분리 -> 틀렸습니다.
+방식 1 : 함수 분리
 """
-# blindColor = ["G", "R"]
-#
-# MAP = []
-# for _ in range(N) :
-#     MAP.append(list(input().rstrip()))
-#
-# VISITED_COMMON = [[False]*N for _ in range(N)]
-# VISITED_BLIND = [[False]*N for _ in range(N)]
-#
-# COMMON_COUNT = 0
-# BLIND_COUNT = 0
+blindColor = ["G", "R"]
+
+MAP = []
+for _ in range(N) :
+    MAP.append(list(input().rstrip()))
+
+VISITED_COMMON = [[False]*N for _ in range(N)]
+VISITED_BLIND = [[False]*N for _ in range(N)]
+
+COMMON_COUNT = 0
+BLIND_COUNT = 0
 
 
 """
 정상인 대상의 영역 계산 BFS 함수
 """
-# def bfsForCommon(y, x, color) :
-#     q = deque()
-#     q.append([y,x])
-#     VISITED_COMMON[y][x] = True
-#     while q :
-#         nowY, nowX = q.popleft()
-#         for i in range(4) :
-#             nextY = nowY + d[i][0]
-#             nextX = nowX + d[i][1]
-#             if 0 <= nextY < N and 0 <= nextX < N and MAP[nextY][nextX] == color and not VISITED_COMMON[nextY][nextX] :
-#                 count += 1
-#                 VISITED_COMMON[nextY][nextX] = True
-#                 q.append([nextY, nextX])
+def bfsForCommon(y, x, color) :
+    q = deque()
+    q.append([y,x])
+    VISITED_COMMON[y][x] = True
+    while q :
+        nowY, nowX = q.popleft()
+        for i in range(4) :
+            nextY = nowY + d[i][0]
+            nextX = nowX + d[i][1]
+            if 0 <= nextY < N and 0 <= nextX < N and MAP[nextY][nextX] == color and not VISITED_COMMON[nextY][nextX] :
+                count += 1
+                VISITED_COMMON[nextY][nextX] = True
+                q.append([nextY, nextX])
 
 """
 색약 장애인 대상의 색구분 결과 반환 함수
 """
-# def isSeemLikeSameArea(searchColor, nextColor):
-#     """
-#     Blue 영역을 계산하는 BFS 중일 때는
-#     기존 방식대로 같은지 비교한 boolean 값
-#
-#     만약 Red 나 Green 일 때는
-#     다음 값이 Red나 Green이면 가능하기 때문에
-#     nextColor가 blindColor 안에 포함되는지에 대한 여부
-#     """
-#     if searchColor == "B":
-#         return searchColor == nextColor
-#     else:
-#         return nextColor in blindColor
+def isSeemLikeSameArea(searchColor, nextColor):
+    """
+    Blue 영역을 계산하는 BFS 중일 때는
+    기존 방식대로 같은지 비교한 boolean 값
 
-# def bfsForColorBlind(y, x, color) :
-#     q = deque()
-#     q.append([y, x])
-#     VISITED_BLIND[y][x] = True
-#     while q :
-#         nowY, nowX = q.popleft()
-#         for i in range(4):
-#             nextY = nowY + d[i][0]
-#             nextX = nowX + d[i][1]
-#             if 0 <= nextY < N and 0 <= nextX < N and isSeemLikeSameArea(color, MAP[nextY][nextX]) and not VISITED_BLIND[nextY][nextX]:
-#                 count += 1
-#                 VISITED_BLIND[nextY][nextX] = True
-#                 q.append([nextY, nextX])
+    만약 Red 나 Green 일 때는
+    다음 값이 Red나 Green이면 가능하기 때문에
+    nextColor가 blindColor 안에 포함되는지에 대한 여부
+    """
+    if searchColor == "B":
+        return searchColor == nextColor
+    else:
+        return nextColor in blindColor
+
+def bfsForColorBlind(y, x, color) :
+    q = deque()
+    q.append([y, x])
+    VISITED_BLIND[y][x] = True
+    while q :
+        nowY, nowX = q.popleft()
+        for i in range(4):
+            nextY = nowY + d[i][0]
+            nextX = nowX + d[i][1]
+            if 0 <= nextY < N and 0 <= nextX < N and isSeemLikeSameArea(color, MAP[nextY][nextX]) and not VISITED_BLIND[nextY][nextX]:
+                count += 1
+                VISITED_BLIND[nextY][nextX] = True
+                q.append([nextY, nextX])
 """
 각 블럭에 대해 
 조건문1 : 정상인 VISITED를 사용한 정상인 영역 구분 연산 
 조건문2 : 색약장애인 VISITED를 사용한 색약 장애인 영역 구분 연산 
 """
-# for i in range(N) :
-#     for k in range(N) :
-#         if not VISITED_COMMON[i][k] :
-#             COMMON_COUNT += 1
-#             bfsForCommon(i, k, MAP[i][k])
-#         elif not VISITED_BLIND[i][k] :
-#             BLIND_COUNT += 1
-#             bfsForColorBlind(i, k, MAP[i][k])
-#
-# print(COMMON_COUNT, BLIND_COUNT, sep=" ")
+for i in range(N) :
+    for k in range(N) :
+        if not VISITED_COMMON[i][k] :
+            COMMON_COUNT += 1
+            bfsForCommon(i, k, MAP[i][k])
+
+        if not VISITED_BLIND[i][k] :
+            BLIND_COUNT += 1
+            bfsForColorBlind(i, k, MAP[i][k])
+
+print(COMMON_COUNT, BLIND_COUNT, sep=" ")
 
 
 """
@@ -143,7 +144,6 @@ for i in range(N) :
         """
         if not VISITED_BLIND[i][k] :
             BLIND_COUNT += 1
-
             BLIND_MAP, VISITED_BLIND =  bfs(i, k, BLIND_MAP[i][k], VISITED_BLIND, BLIND_MAP)
         if not VISITED_COMMON[i][k] :
             COMMON_COUNT += 1
--- yummygyudon/DFS+BFS/day5/3184_양.py
+++ yummygyudon/DFS+BFS/day5/3184_양.py
@@ -30,6 +30,10 @@ def bfs(sheep_y, sheep_x) :
     CHECKED[sheep_y][sheep_x] = True
     while q:
         y, x = q.popleft()
+        """
+        해당 위치가 양이라면 sheepCount에 누적합
+        해당 위치가 늑대라면 wolfCount에 누적합
+        """
         if MAP[y][x] == SHEEP :
             sheepCount += 1
         elif MAP[y][x] == WOLF :
@@ -37,24 +41,30 @@ def bfs(sheep_y, sheep_x) :
         for i in range(4):
             nextY = y + d[i][0]
             nextX = x + d[i][1]
+            """
+            다음칸이 울타리(HEDGE)가 아니면서 -> 각 bfs마다 같은 영역을 돌 수 있음
+            & 처음 방문하는 경우
+            """
             if (0 <= nextY < R) and (0 <= nextX < C) and not MAP[nextY][nextX] == HEDGE and not CHECKED[nextY][nextX] :
                 CHECKED[nextY][nextX] = True
                 q.append([nextY, nextX])
     # print(sheepCount, wolfCount, sep=" ")
+    """
+    양 수와 늑대 수를 비교한 boolean 값을 활용해서 살아남은 개체에 대해 dict의 값에 누적합
+    
+    양의 수 > 늑대의 수 -> 늑대는 더하지 않음
+    늑대의 수 > 양의 수 -> 양은 더하지 않음
+    """
     sheepAlive = (sheepCount > wolfCount)
     if not sheepAlive :
         STATE[WOLF] = STATE.get(WOLF) + wolfCount
     else :
         STATE[SHEEP] = STATE.get(SHEEP) + sheepCount
 
+
 for i in range(R) :
     for k in range(C) :
         if (MAP[i][k] == SHEEP or MAP[i][k] == WOLF) and not CHECKED[i][k] :
             bfs(i, k)
-
-"""
-양의 수 > 늑대의 수 -> 늑대 삭제
-늑대의 수 > 양의 수 -> 양 삭제
-"""
 
 print(STATE[SHEEP], STATE[WOLF], sep=" ")
