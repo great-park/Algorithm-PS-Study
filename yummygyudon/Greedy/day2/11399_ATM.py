61d4e44f7890f5706d26bd537bf574e423e8136b
yummygyudon <bang2brew@gmail.com>
2023-02-15 14:51:30 +0900

    FEAT($Greedy) : 11399번 ATM 문제 완료

---
+++ yummygyudon/Greedy/day2/11399_ATM.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+TIMES = list(map(int, input().split()))
+TIMES.sort()
+RESULT= [0]*(N+1)
+
+"""
+누적합 시켜주기
+"""
+for i in range(N) :
+    RESULT[i+1] = RESULT[i]+TIMES[i]
+"""
+s = 0
+for i in range(N) :
+    for k in range(i+1):
+        s += time[k]
+"""
+print(sum(RESULT))
