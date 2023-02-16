7ca8a290dedbe751aef9367d9de83c0ca8f2a006
great-park <cksgh1735@gmail.com>
2023-02-17 02:50:39 +0900

    fix: 주석 추가

--- great-park/Greedy/1946 신입 사원.py
+++ great-park/Greedy/1946 신입 사원.py
@@ -6,19 +6,19 @@ for i in range(T):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(N)]
     data.sort()  # 서류 성적순
-
-  """
-  1 4
-  2 3
-  3 2
-  4 1
-  5 5
-  """
     result = 1
     # 면접 점수 평가
     min = data[0][1]
     for k in range(1, N):
-        if data[k][1] < min:
+        if data[k][1] < min:  # 서류에서 졌으므로 면접에서 이겨야 채용됨
             min = data[k][1]
             result += 1
     print(result)
+
+"""
+1 4
+2 3
+3 2
+4 1
+5 5
+"""
