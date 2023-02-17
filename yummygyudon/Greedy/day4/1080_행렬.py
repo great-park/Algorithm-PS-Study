edd181350205e86f3514a6f08e9bb4ad678850d6
yummygyudon <bang2brew@gmail.com>
2023-02-17 21:04:12 +0900

    STYLE($Greedy) : 1080행렬 문제 파일 변수 명명 방식 변환

--- yummygyudon/Greedy/day4/1080_행렬.py
+++ yummygyudon/Greedy/day4/1080_행렬.py
@@ -1,25 +1,47 @@
 import sys
 input = sys.stdin.readline
 
-h, w = map(int,input().split())
-arr_A = []
-arr_B = []
-for _ in range(h) :
-    arr_A.append(list(map(int,list(input()))))
-for _ in range(h) :
-    arr_B.append(list(map(int,list(input()))))
+H, W = map(int, input().split())
+MATRIX_A = []
+MATRIX_B = []
+for _ in range(H) :
+    MATRIX_A.append(list(map(int, input().rstrip())))
+for _ in range(H) :
+    MATRIX_B.append(list(map(int, input().rstrip())))
 def change(x, y) :
     for i in range(x, x+3) :
-        for j in range(y, y+3) :
-            arr_A[i][j] = 1-arr_A[i][j]
-cnt = 0
-for move_d in range(h-3+1) :
-    for move_r in range(w-3+1):
-        if arr_A[move_d][move_r] != arr_B[move_d][move_r] :
-            cnt+=1
+        for k in range(y, y+3) :
+            """
+            1 - 1 : 0
+            1 - 0 : 1
+            """
+            MATRIX_A[i][k] = 1 - MATRIX_A[i][k]
+
+"""
+range에 음수가 들어갈 경우
+알아서 for문을 돌지 않음
+-> 딱 길이가 3 이상일 때만 for문 작용
+-> 3 x 2더라도 2인 부분에서 for문이 안돌기 때문에 skip 됨
+"""
+"""
+틀린걸 발견할 때마다 
+해당 지점부터 3x3영역이 존재한다면 지점으로부터 3x3 영역 뒤집어 버리기
+"""
+COUNT = 0
+for move_d in range(H - 3 + 1) :
+    for move_r in range(W - 3 + 1):
+        if MATRIX_A[move_d][move_r] != MATRIX_B[move_d][move_r] :
             change(move_d, move_r)
-for i in range(h) :
-    for j in range(w):
-        if arr_A[i][j] != arr_B[i][j] :
-            cnt= -1
-print(cnt)
+            COUNT += 1
+"""
+뒤집는 처리 이후에도
+틀린 게 있으면 불가능하다는 것 -> -1로 바꾸기
+"""
+for i in range(H) :
+    for k in range(W):
+        if MATRIX_A[i][k] != MATRIX_B[i][k] :
+            COUNT = -1
+print(COUNT)
+
+# for i in range(-2) :
+#     print("음수여도 출력")
